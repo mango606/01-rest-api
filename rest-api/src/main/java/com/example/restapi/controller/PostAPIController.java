51eea0b7a61ec2545f62314b2452b1bd17e7ae7f
mango606 <yuj08184@kangwon.ac.kr>
2024-04-28 18:57:54 +0900

    feat: REST API-POST-01
    Request Body 데이터를 받을 수 있는 방식

---
+++ rest-api/src/main/java/com/example/restapi/controller/PostAPIController.java
@@ -0,0 +1,23 @@
+package com.example.restapi.controller;
+
+import com.example.restapi.model.BookRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PostAPIController {
+
+    // http://localhost:8080/api/post
+    @PostMapping("/post")
+    public BookRequest post(
+        @RequestBody BookRequest bookRequest
+    ) {
+        System.out.println(bookRequest);
+        return bookRequest; // 객체를 기본적으로 json으로 변환해줌.
+    }
+
+    // RequestBody로 사용자의 이름, 전화번호, 이메일을 받는 POST Method 만들기 (User 클래스 생성, 주소 지정, 해당 데이터 받기)
+}
