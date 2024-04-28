777e223def45f7c1014e75c0581e03039e2ca324
mango606 <yuj08184@kangwon.ac.kr>
2024-04-28 13:00:01 +0900

    feat: REST API-GET-02
    message 값 출력

--- rest-api/src/main/java/com/example/restapi/controller/RestApiController.java
+++ rest-api/src/main/java/com/example/restapi/controller/RestApiController.java
@@ -1,6 +1,7 @@
 package com.example.restapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,20 @@ public class RestApiController {
     public String hello() {
         var html = "<html> <body> <h1> Hello Spring Boot </h1> </body> </html>";
         return html;
+    }
+
+    @GetMapping(path = "/echo/{message}")
+    public String echo(
+            @PathVariable String message    // PathVariable(name = "message") 를 사용하면 변수명이 message가 아니어도 매칭됨.
+    ) {
+        System.out.println("echo message: " + message);
+
+        // TODO: 대문자로 변환해서 Return
+
+        // String 타입의 변수 외에 다른 타입 받아보기
+
+        // boolean, integer
+
+        return message;
     }
 }
