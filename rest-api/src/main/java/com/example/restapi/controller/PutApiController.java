9b1c4b82e50904417a347bcadada9bd4df268bbd
mango606 <yuj08184@kangwon.ac.kr>
2024-04-29 13:59:28 +0900

    feat: Response Entity

--- rest-api/src/main/java/com/example/restapi/controller/PutApiController.java
+++ rest-api/src/main/java/com/example/restapi/controller/PutApiController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("/api")
-public class PutAPIController {
+public class PutApiController {
 
     // http://localhost:8080/api/put
     @PutMapping("/put")
